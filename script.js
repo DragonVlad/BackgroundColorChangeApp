0193a1643114c1f67e009d439c851c07c451ca56
DragonVlad <55784731+DragonVlad@users.noreply.github.com>
2025-11-29 12:27:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let bodyColor = document.body;
 
 function randColor() {
-                let r = Math.floor(Math.random() * (256)),
-                g = Math.floor(Math.random() * (256)),
-                b = Math.floor(Math.random() * (256)),
-                color = '#' + r.toString(16) + g.toString(16) + b.toString(16);
+    let r = Math.floor(Math.random() * (256)),
+        g = Math.floor(Math.random() * (256)),
+        b = Math.floor(Math.random() * (256)),
+        color = '#' + r.toString(16) + g.toString(16) + b.toString(16);
 
-		    bodyColor.style.background = color;
+	bodyColor.style.background = color;
 }
 
 bodyColor.addEventListener('keydown', function(){
